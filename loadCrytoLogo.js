ed21f10084d8114696c61c2e1f6560f0cac8382f
JingTings <91300350+JingTings@users.noreply.github.com>
2021-09-30 11:32:54 +0800

    Create loadCrytoLogo.js

---
+++ loadCrytoLogo.js
@@ -0,0 +1,25 @@
+function loadCrytoLogo() {
+	/* Blue Container */
+	let cryto_logo_bg_outer = document.createElement('div');
+  	cryto_logo_bg_outer.setAttribute("class", "cryto-logo-bg-outer");
+  	document.getElementById('body').appendChild(cryto_logo_bg_outer); //Change body to parent element
+    
+	/* White Container */
+  let cryto_logo_bg_inner = document.createElement('div');
+  	cryto_logo_bg_inner.setAttribute("class", "cryto-logo-bg-inner");
+		document.getElementsByClassName("cryto-logo-bg-outer")[0].appendChild(cryto_logo_bg_inner);
+    
+	/* Gray Border Container - Prevent Clipping*/
+  let cryto_logo_container = document.createElement('div');
+  	cryto_logo_container.setAttribute("class", "cryto-logo-container");
+		document.getElementsByClassName("cryto-logo-bg-inner")[0].appendChild(cryto_logo_container);
+
+	/* Logo */
+  let cryto_logo = document.createElement('img');
+  	cryto_logo.src = 
+  'https://cryptologos.cc/logos/harmony-one-logo.svg?v=013'; //Link to Image or API
+  	cryto_logo.setAttribute("class", "cryto-logo");
+		document.getElementsByClassName("cryto-logo-container")[0].appendChild(cryto_logo);
+}
+
+loadCrytoLogo();
